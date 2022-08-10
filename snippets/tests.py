e3dad103b26a0fe128e403b6552df562394de29c
akm-ksk <akmmst0496@gmail.com>
2022-08-10 17:36:45 +0900

    下層のテンプレート test ビューの制作

--- snippets/tests.py
+++ snippets/tests.py
@@ -1,44 +1,129 @@
 from django.http import HttpRequest
-from django.test import TestCase
+from django.test import TestCase, Client, RequestFactory
 from django.urls import resolve
+from django.contrib.auth import get_user_model
 
-from snippets.views import snippet_new, snippet_detail, snippet_edit
+from snippets.views import top, snippet_new, snippet_detail, snippet_edit
+from snippets.models import Snippet
+
+UserModel = get_user_model()
 
 
 # Create your tests here.
-class CreateSnippetsTest(TestCase):
-    def test_should(self):
-        found = resolve('/snippets/new/')
-        self.assertEqual(snippet_new, found.func)
+# class TopPageViewTest(TestCase):
+#     def CreateSnippetsTest(self):
+#         # request = HttpRequest()
+#         # response = top(request)
+#         response = self.client.get('/')
+#         self.assertEqual(response.content, b'Hello World')
+#
+#     def test_top_return_200(self):
+#         # request = HttpRequest()  # リクエストのオブジェクトを生成
+#         # response = top(request)
+#         response = self.client.get('/')
+#         self.assertEqual(response.status_code, 200)
+#
+#     def test_top_returns_expected_content(self):
+#         # request = HttpRequest()
+#         # response = top(request)
+#         response = self.client.get('/')
+#         self.assertEqual(response.content, b'Hello World')
 
+class TopPageTest(TestCase):
+    def test_top_page_returns_200_and_expected_title(self):
+        response = self.client.get('/')
+        self.assertContains(response, 'Djangoスニペット', status_code=200)
+
+    def test_top_page_uses_expected_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'snippets/top.html')
+
+
+class TopPageRenderSnippetsTest(TestCase):
+    def setUp(self):
+        self.user = UserModel.objects.create(
+            username='test_user',
+            email='test@example.com',
+            password='top_secret_pass0001',
+        )
+        self.snippets = Snippet.objects.create(
+            title='title01',
+            code='print("Hellow")',
+            description='description1',
+            create_by=self.user,
+        )
+
+    def test_should_return_snippet_title(self):
+        request = RequestFactory().get('/')
+        request.user = self.user
+        response = top(request)
+        self.assertContains(response, self.snippets.title)
+
+    def test_should_return_username(self):
+        request = RequestFactory().get('/')
+        request.user = self.user
+        response = top(request)
+        self.assertContains(response, self.user.username)
+
+
+# class CreateSnippetsTest(TestCase):
+#     def test_should(self):
+#         found = resolve('/snippets/new/')
+#         self.assertEqual(snippet_new, found.func)
+class CreateSnippetsTest(TestCase):
+    def serUp(self):
+        self.user = UserModel.objects.create(
+            username='test_user',
+            email='test@example.com',
+            password='top_secret_pass0001',
+        )
+        self.client.force_login(self.user)  # ログインする
+
+    def test_render_create_form(self):
+        response = self.client.get('/snippets/new/')
+        self.assertContains(response, 'スニペットの登録', status_code=200)
+
+    def test_create_snippet(self):
+        data = {
+            'title': 'タイトル',
+            'code': 'コード',
+            'description': '解説'
+        }
+        self.client.post('/snippets/new/', data)
+        snippet = Snippet.objects.get(title='タイトル')
+        self.assertEqual('コード', snippet.code)
+        self.assertEqual('解説', snippet.description)
+
+
+# class SnippetsDetailTest(TestCase):
+#     def test_should_resolve_snippet_detail(self):
+#         found = resolve('/snippets/1/')
+#         self.assertEqual(snippet_detail, found.func)
 
 class SnippetsDetailTest(TestCase):
-    def test_should_resolve_snippet_detail(self):
-        found = resolve('/snippets/1/')
-        self.assertEqual(snippet_detail, found.func)
+    def setUp(self):
+        self.user = UserModel.objects.create(
+            username='test_user',
+            email='test@example.com',
+            password='detail_secret_pass0001',
+        )
+        self.snippet = Snippet.objects.create(
+            title='タイトル',
+            code='コード',
+            description='解説',
+            create_by=self.user,
+        )
+
+    def test_should_use_expected_template(self):
+        response = self.client.get('/snippets/%s/' % self.snippet.id)
+        self.assertTemplateUsed(response, 'snippets/snippet_detail.html')
+
+    def test_top_page_returns_200_and_expected_heading(self):
+        response = self.client.get('/snippets/%s/' % self.snippet.id)
+        self.assertContains(response, self.snippet.title, status_code=200)
 
 
 class EditSnippetsTest(TestCase):
     def test_should_resolve_snippet_edit(self):
         found = resolve('/snippets/1/edit/')
         self.assertEqual(snippet_edit, found.func)
-
-
-class TopPageViewTest(TestCase):
-    def CreateSnippetsTest(self):
-        # request = HttpRequest()
-        # response = top(request)
-        response = self.client.get('/')
-        self.assertEqual(response.content, b'Hello World')
-
-    def test_top_return_200(self):
-        # request = HttpRequest()  # リクエストのオブジェクトを生成
-        # response = top(request)
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
-
-    def test_top_returns_expected_content(self):
-        # request = HttpRequest()
-        # response = top(request)
-        response = self.client.get('/')
-        self.assertEqual(response.content, b'Hello World')
--- snippets/views.py
+++ snippets/views.py
@@ -1,18 +1,54 @@
-from django.http import HttpResponse
-
+from django.http import HttpResponse, HttpResponseForbidden
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+from django.shortcuts import render, get_object_or_404, redirect
+
+from snippets.forms import SnippetForm
+from snippets.models import Snippet
+
+
 def top(request):
-    return HttpResponse(b'Hello World')
+    snippets = Snippet.objects.all()
+    context = {'snippets': snippets}
+    return render(request, 'snippets/top.html', context)
 
 
+# def snippet_new(request):
+#     return HttpResponse('スニペットの登録')
+@login_required
 def snippet_new(request):
-    return HttpResponse('スニペットの登録')
+    if request.method == 'POST':
+        form = SnippetForm(request.POST)
+        if form.is_valid():
+            snippet = form.save(commit=False)
+            snippet.create_by = request.user
+            snippet.save()
+            return redirect(snippet_detail, snippet_id=snippet.pk)
+    else:
+        form = SnippetForm()
+    return render(request, 'snippets/snippet_new.html', {'form': form})
 
 
-def snippet_edit(request):
-    return HttpResponse('スニペットの編集')
+# def snippet_edit(request):
+#     return HttpResponse('スニペットの編集')
+
+@login_required
+def snippet_edit(request, snippet_id):
+    snippet = get_object_or_404(Snippet, pk=snippet_id)
+    if snippet.create_by_id != request.user.id:
+        return HttpResponseForbidden('このスニッペトの編集は許可されていません。')
+
+    if request.method == 'POST':
+        form = SnippetForm(request.POST, instance=snippet)
+        if form.valid():
+            form.save()
+            return redirect('snippet_detail', snippet_id=snippet_id)
+    else:
+        form = SnippetForm(instance=snippet)
+    return render(request, 'snippets/snippet_edit.html', {'form': form})
 
 
-def snippet_detail(request):
-    return HttpResponse('スニペットの詳細画面')
+def snippet_detail(request, snippet_id):
+    snippet = get_object_or_404(Snippet, pk=snippet_id)
+    return render(request, 'snippets/snippet_detail.html', {'snippet': snippet})
